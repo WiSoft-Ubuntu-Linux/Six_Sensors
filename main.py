0388b5f0ca98854db70d580ed6e6ea19edb9cddd
Ksyear <ksy0305107@naver.com>
2025-10-01 20:30:48 +0900

    sql

--- main.py
+++ main.py
@@ -120,19 +120,3 @@ finally:
     UART_BUS.close()
   if dht_device:
     dht_device.exit()
-
-# try:
-#   while True:
-#     try:
-#       temperature = dht_device.temperature
-#       humidity = dht_device.humidity
-#       print(f"Temperature = {temperature:0.1f}*C Humidity = {humidity:0.1f}%")
-#     except RuntimeError:
-#       time.sleep(1.0)
-
-#     time.sleep(2.0)
-
-# except KeyboardInterrupt:
-#   print("I'm done!")
-# finally:
-#   dht_device.exit()
